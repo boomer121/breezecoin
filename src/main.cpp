40e200c42d2d05802a6b30667c24671d9459d185
Root <majorprobes@gmail.com>
2014-02-17 13:04:59 +0000

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -3308,7 +3308,7 @@ bool InitBlockIndex() {
 #endif
 #else
                 // Generic scrypt
-                scrypt_N_1_1_256_sp_generic(BEGIN(block.nVersion), BEGIN(thash), scratchpad, GetNfactor(pblock->nTime));
+                scrypt_N_1_1_256_sp_generic(BEGIN(block.nVersion), BEGIN(thash), scratchpad);
 #endif
                 if (thash <= hashTarget)
                     break;
