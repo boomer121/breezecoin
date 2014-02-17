809e4ef2c1f2ce0d9602f50ca5259038cf801f77
Root <majorprobes@gmail.com>
2014-02-17 13:03:38 +0000

    change

--- src/main.cpp
+++ src/main.cpp
@@ -3301,10 +3301,10 @@ bool InitBlockIndex() {
                 // it is faster to use directly than to use a function pointer or conditional.
 #if defined(_M_X64) || defined(__x86_64__) || defined(_M_AMD64) || (defined(MAC_OSX) && defined(__i386__))
                 // Always SSE2: x86_64 or Intel MacOS X
-                scrypt_1024_1_1_256_sp_sse2(BEGIN(block.nVersion), BEGIN(thash), scratchpad);
+                scrypt_N_1_1_256_sp_sse2(BEGIN(block.nVersion), BEGIN(thash), scratchpad);
 #else
                 // Detect SSE2: 32bit x86 Linux or Windows
-                scrypt_1024_1_1_256_sp(BEGIN(block.nVersion), BEGIN(thash), scratchpad);
+                scrypt_N_1_1_256_sp(BEGIN(block.nVersion), BEGIN(thash), scratchpad);
 #endif
 #else
                 // Generic scrypt
